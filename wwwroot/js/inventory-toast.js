8843d1e44c7f57a7b34a35875004573f03c2d4c6
DESKTOP-FCGA3DM\milan <milance.za019@gmail.com>
2025-07-25 09:19:09 +0200

    Combine toast scripts for easier include in script tag for shopify

--- wwwroot/js/inventory-toast.js
+++ wwwroot/js/inventory-toast.js
@@ -1,5 +1,50 @@
 ﻿console.log("loaded inventory-toast.js");
 
+class ToastService {
+    constructor(options = {}) {
+        this.defaultOptions = {
+            duration: 3000,
+            close: true,
+            gravity: "top",
+            position: "right",
+            backgroundColor: "green",
+            stopOnFocus: true,
+            ...options
+        };
+    }
+
+    show(message, type = "success") {
+        if (!message) { return; }
+
+        const bgColor = this.getColor(type);
+
+        Toastify({
+            text: message,
+            bgColor,
+            ...this.defaultOptions
+        }).showToast();
+    }
+
+    success(message) {
+        this.show(message, "success");
+    }
+
+    failure(message) {
+        this.show(message, "failure");
+    }
+
+    getColor(type) {
+        switch (type) {
+            case "success": return "green";
+            case "failure": return "red";
+            default: return "grey";
+        }
+    }
+
+}
+
+window.Toast = new ToastService();
+
 (async () => {
     const handle = window.location.pathname.split("/products")[1];
     if (!handle) {
