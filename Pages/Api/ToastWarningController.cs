5cc48582b74c62757846aff67570497e42155751
DESKTOP-FCGA3DM\milan <milance.za019@gmail.com>
2025-07-31 11:05:19 +0200

    Add frontend logic for re-fetching understocked products every 30 seconds

---
+++ Pages/Api/ToastWarningController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StockNotificationWarning.Services.Abstraction;
+
+namespace StockNotificationWarning.Pages.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToastWarningController(IInventoryMonitorService inventoryMonitor,
+                                        IShopifyContextService shopifyContextService,
+                                        IToastNotificationService toastService) : ControllerBase
+    {
+        readonly IInventoryMonitorService _inventoryMonitor = inventoryMonitor;
+        readonly IShopifyContextService _shopifyContextService = shopifyContextService;
+        readonly IToastNotificationService _toastService = toastService;
+        
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            await _shopifyContextService.InitializeAsync(HttpContext);
+            await _inventoryMonitor.NotifyToastServiceOfUnderstocked();
+            var toasts = _toastService.GetAllToasts();
+
+            return Ok(toasts);
+        }
+    }
+}
