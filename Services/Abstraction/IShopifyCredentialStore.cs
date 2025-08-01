d6afaf18ead006df74a00449ff4b8c4dc705fbdd
DESKTOP-FCGA3DM\milan <milance.za019@gmail.com>
2025-08-01 15:53:04 +0200

    Remove unused legacy implementation of ShopifyCredentialStore

--- Services/Abstraction/IShopifyCredentialStore.cs
+++ Services/Abstraction/IShopifyCredentialStore.cs
@@ -4,7 +4,5 @@
     {
         void Set(HttpContext context, string? shopKey = null, string? tokenValue = null);
         string? Get(string shopKey);
-
-        void InitializeAsync(HttpContext context, string? shop = null, string? token = null);
     }
 }
--- Services/ShopifyCredentialStore.cs
+++ Services/ShopifyCredentialStore.cs
@@ -2,24 +2,9 @@
 
 namespace StockNotificationWarning.Services
 {
-    public class ShopifyCredentialStore(ILogger<ShopifyCredentialStore> logger) : IShopifyCredentialStore
+    public class ShopifyCredentialStore : IShopifyCredentialStore
     {
-        readonly ILogger<ShopifyCredentialStore> _logger = logger;
-        public string? Shop { get; private set; }
-
         readonly Dictionary<string, string> _store = new();
-
-        public string? AccessToken { get; private set; }
-
-        public void InitializeAsync(HttpContext context,
-                                          string? shop = null,
-                                          string? token = null)
-        {
-            Shop = shop ?? context.Request.Query["shop"];
-            AccessToken = token ?? context.Request.Headers["Authorization"].ToString().Replace("Bearer ", "");
-
-            _logger.LogInformation("INIT ASYNC CALLED FOR SHOPIFY CONTEXT");
-        }
 
         public void Set(HttpContext context, string? shopKey = null, string? tokenValue = null)
         {
